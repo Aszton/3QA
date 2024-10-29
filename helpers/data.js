afba968f56b682e2d1303b0d6fec5f315f1f960c
Aszton <bartoszkuczera88@gmail.com>
2024-10-29 19:08:26 +0100

    base_code

---
+++ helpers/data.js
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+export const baseURL = "https://demoqa.com";
+export const userID = "443b578e-26d3-4597-bcd9-b1d96b32d745"
+export const isbn = "9781491904244"
+export const password = process.env.PASSWORD
